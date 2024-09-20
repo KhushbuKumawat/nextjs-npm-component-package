18a6be5cf7b39dd96482c90bf29855cc8534c56f
KhushbuKumawat <shree331027@gmail.com>
2024-09-20 12:01:07 +0530

    first commit

---
+++ rollup.config.mjs
@@ -0,0 +1,42 @@
+// rollup.config.js
+import { defineConfig } from 'rollup';
+import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import terser from '@rollup/plugin-terser';
+
+export default defineConfig({
+  input: 'src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+  external: ['react', 'react-dom'] ,
+  plugins: [
+    peerDepsExternal(),
+    resolve(),
+    commonjs(),
+    terser(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**',
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    }),
+    postcss({
+      modules: true, 
+      extensions: ['.css'],
+      extract: true,  // Extract CSS into separate file
+      minimize: true, // Minify CSS
+    }),
+  ],
+});
